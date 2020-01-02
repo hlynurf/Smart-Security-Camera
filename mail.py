d533666d711550bacee023644ffa67fba082f562
Hlynur Jónsson <hlynur4@gmail.com>
2020-01-02 21:48:29 +0000

    Python3 fixes

--- mail.py
+++ mail.py
@@ -1,16 +1,19 @@
+import os
 import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
-from email.MIMEImage import MIMEImage
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 
 # Email you want to send the update from (only works with gmail)
-fromEmail = 'email@gmail.com'
+fromEmail = 'hlynur4@gmail.com'
 # You can generate an app password here to avoid storing your password in plain text
 # https://support.google.com/accounts/answer/185833?hl=en
-fromEmailPassword = 'password'
+fromEmailPassword = os.environ('GMAIL_PASS')
 
 # Email you want to send the update to
-toEmail = 'email2@gmail.com'
+toEmail = 'hlynur4@gmail.com'
+
 
 def sendEmail(image):
 	msgRoot = MIMEMultipart('related')
--- main.py
+++ main.py
@@ -20,6 +20,7 @@ app.config['BASIC_AUTH_FORCE'] = True
 basic_auth = BasicAuth(app)
 last_epoch = 0
 
+
 def check_for_objects():
 	global last_epoch
 	while True:
@@ -27,30 +28,33 @@ def check_for_objects():
 			frame, found_obj = video_camera.get_object(object_classifier)
 			if found_obj and (time.time() - last_epoch) > email_update_interval:
 				last_epoch = time.time()
-				print "Sending email..."
+				print("Sending email...")
 				sendEmail(frame)
-				print "done!"
-		except:
-			print "Error sending email: ", sys.exc_info()[0]
+				print("done!")
+		except Exception:
+			print("Error sending email: ", sys.exc_info()[0])
+
 
 @app.route('/')
 @basic_auth.required
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
+
 
 def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+	while True:
+		frame = camera.get_frame()
+		yield (b'--frame\r\n'
+			   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(video_camera),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+	return Response(gen(video_camera),
+					mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == '__main__':
-    t = threading.Thread(target=check_for_objects, args=())
-    t.daemon = True
-    t.start()
-    app.run(host='0.0.0.0', debug=False)
+	t = threading.Thread(target=check_for_objects, args=())
+	t.daemon = True
+	t.start()
+	app.run(host='0.0.0.0', debug=False)
