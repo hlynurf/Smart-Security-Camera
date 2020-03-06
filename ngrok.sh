52bb0502f61fbb917e5307b26ad4c6019fe82ef6
Hlynur Jónsson <hlynur4@gmail.com>
2020-03-06 22:47:53 +0000

    fix ngrok

--- ngrok.sh
+++ ngrok.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
 unzip ngrok-stable-linux-386.zip
